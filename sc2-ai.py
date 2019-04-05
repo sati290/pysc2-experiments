5f99d2151db8d48e114e88de23183581d1445a38
Attila Simon <sati290@gmail.com>
2019-04-05 21:11:00 +0200

    resume support, command line improvements, mean rewards summary

--- sc2-ai.py
+++ sc2-ai.py
@@ -1,5 +1,6 @@
 import time
 from os import path
+from collections import deque
 from absl import app, flags
 import numpy as np
 import gin
@@ -12,22 +13,34 @@ from agent import A2CAgent
 from utils import print_parameter_summary, LogProgressHook
 
 FLAGS = flags.FLAGS
-
-flags.DEFINE_boolean('save_checkpoints', False, '')
-flags.DEFINE_boolean('profile', False, '')
+flags.DEFINE_integer('save_checkpoint_secs', None, '')
+flags.DEFINE_alias('s', 'save_checkpoint_secs')
+flags.DEFINE_integer('save_checkpoint_steps', None, '')
+flags.DEFINE_string('resume', None, '')
+flags.DEFINE_alias('r', 'resume')
 flags.DEFINE_integer('step_limit', None, '', lower_bound=0)
+flags.DEFINE_alias('l', 'step_limit')
+flags.DEFINE_string('run_name', None, '')
+flags.DEFINE_alias('n', 'run_name')
 flags.DEFINE_string('config', 'config.gin', '')
+flags.DEFINE_alias('c', 'config')
 flags.DEFINE_boolean('gpu_memory_allow_growth', False, '')
 flags.DEFINE_float('gpu_memory_fraction', None, '', lower_bound=0, upper_bound=1)
+flags.DEFINE_boolean('profile', False, '')
 flags.DEFINE_boolean('debug', False, '')
 flags.DEFINE_boolean('trace', False, '')
 
 
 @gin.configurable
 def main(args):
-    output_dir = path.join('runs', time.strftime('%Y%m%d-%H%M%S', time.localtime()))
+    if FLAGS.resume:
+        output_dir = FLAGS.resume
+        gin.parse_config_file(path.join(output_dir, 'operative_config-0.gin'))
+    else:
+        run_name = FLAGS.run_name or time.strftime('%Y%m%d-%H%M%S', time.localtime())
+        output_dir = path.join('runs', run_name)
+        gin.parse_config_file(FLAGS.config)
 
-    gin.parse_config_file(FLAGS.config)
     gin.finalize()
 
     with SC2Environment() as env:
@@ -51,9 +64,11 @@ def main(args):
         else:
             hooks.append(tf.train.NanTensorHook(actor.loss))
         with tf.train.MonitoredTrainingSession(config=config, hooks=hooks, checkpoint_dir=output_dir,
-                                               save_checkpoint_secs=3600 if FLAGS.save_checkpoints else None) as sess:
+                                               save_checkpoint_secs=FLAGS.save_checkpoint_secs,
+                                               save_checkpoint_steps=FLAGS.save_checkpoint_steps) as sess:
 
             summary_writer = tf.summary.FileWriterCache.get(output_dir)
+            episode_rewards = deque(maxlen=50)
 
             env.start()
 
@@ -75,9 +90,13 @@ def main(args):
                     obs = sess.run_step_fn(step_fn)
                     episode_sum_reward += obs[0].reward
 
+                episode_rewards.append(episode_sum_reward)
                 global_step = sess.run_step_fn(lambda step_context: step_context.session.run(tf.train.get_global_step()))
                 summary_writer.add_summary(
-                    tf.Summary(value=[tf.Summary.Value(tag='episode_reward', simple_value=episode_sum_reward)]),
+                    tf.Summary(value=[
+                        tf.Summary.Value(tag='episode_reward', simple_value=episode_sum_reward),
+                        tf.Summary.Value(tag='episode_reward_mean', simple_value=np.asarray(episode_rewards).mean())
+                    ]),
                     global_step=global_step
                 )
 

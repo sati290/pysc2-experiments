1ac59a9c35d35c2ef3008512bdf89328b552dd99
Attila Simon <sati290@gmail.com>
2019-04-17 11:56:16 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sc2-ai',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Attila Simon',
+    author_email='',
+    description='',
+    install_requires=[]
+)

d9f04b2c0a142d586baa70c700020a1e9e2bb7f9
Attila Simon <sati290@gmail.com>
2019-04-25 22:11:23 +0200

    better initialization for layers in fullyconv model

--- sc2ai/models/fullyconv.py
+++ sc2ai/models/fullyconv.py
@@ -11,14 +11,14 @@ from .common import preprocess_spatial_observation
 def spatial_stream(obs, spec):
     x = preprocess_spatial_observation(obs, spec)
     x = Concatenate(1)(x)
-    x = Conv2D(16, 1, activation='linear', data_format='channels_first')(x)
+    x = Conv2D(16, 1, activation='linear', data_format='channels_first', kernel_initializer=tf.keras.initializers.Orthogonal())(x)
 
     return x
 
 
 @gin.configurable
 def value_output(state, activation='linear'):
-    x = Dense(1, activation=activation)(state)
+    x = Dense(1, activation=activation, kernel_initializer=tf.keras.initializers.Orthogonal(gain=0.1))(state)
     return tf.squeeze(x)
 
 
@@ -30,17 +30,17 @@ class FullyConvModel:
                                for name, spec in env_spec.observation_spec.items() if spec.is_spatial}
 
             fc = Concatenate()([Flatten()(x) for x in spatial_streams.values()])
-            fc = Dense(256, activation='relu')(fc)
+            fc = Dense(256, activation='relu', name='fc', kernel_initializer=tf.keras.initializers.Orthogonal())(fc)
 
             with tf.name_scope('policy'):
                 self.policy = {}
                 for name, spec in env_spec.action_spec.items():
                     with tf.name_scope(name):
                         if spec.obs_space:
-                            logits = Conv2D(1, 1, activation='linear', data_format='channels_first')(spatial_streams[spec.obs_space])
+                            logits = Conv2D(1, 1, activation='linear', data_format='channels_first', kernel_initializer=tf.keras.initializers.Orthogonal(gain=0.1))(spatial_streams[spec.obs_space])
                             logits = Flatten()(logits)
                         else:
-                            logits = Dense(np.prod(spec.sizes), activation='linear')(fc)
+                            logits = Dense(np.prod(spec.sizes), activation='linear', kernel_initializer=tf.keras.initializers.Orthogonal(gain=0.1))(fc)
 
                         if name == 'function_id':
                             logits = tf.where(observations['available_actions'] > 0, logits, -1000 * tf.ones_like(logits), name='mask_unavailable_functions')
